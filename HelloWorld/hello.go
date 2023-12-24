8095fe83983561502f2d759e3bf502fbbf1759b9
swananana <swanasimran977@gmail.com>
2023-12-24 19:54:48 +0530

    Hello World in different languages

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -3,14 +3,25 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return spanishPrefix + name 
+	}
+
+	if language == "French" {
+		return frenchPrefix + name
+	}
+	
 	return englishPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
