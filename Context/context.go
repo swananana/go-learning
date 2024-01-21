4332875e0178cbf5bda3a890dff770282cd1f6ec
swananana <swanasimran977@gmail.com>
2024-01-21 22:05:47 +0530

    Context in go

---
+++ Context/context.go
@@ -0,0 +1,40 @@
+package Context
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func Server(store *SpyStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
